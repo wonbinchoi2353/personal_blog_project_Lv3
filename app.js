a7c4715137b5e8f0c4f28a80a9683232737dfc33
wonbin choi <fnffnqls@naver.com>
2023-07-10 16:54:45 +0900

    fix: cookieParser 추가, /api 미들웨어 추가

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const usersRouter = require('./routes/users.route');
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use('/api', [usersRouter]);
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 실행되었습니다.');
