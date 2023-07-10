6b4719d5b9d320c6773cc4b0d54c71fc6e4d7566
wonbin choi <fnffnqls@naver.com>
2023-07-10 22:52:50 +0900

    feat: postRouter 추가

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const usersRouter = require('./routes/users.route');
+const postsRouter = require('./routes/posts.route');
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [usersRouter]);
+app.use('/api', [usersRouter, postsRouter]);
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 실행되었습니다.');
