ba62ffe9314742eaeb6bb82856002fb9a1c6d159
wonbin choi <fnffnqls@naver.com>
2023-07-11 16:21:07 +0900

    feat: 댓글 라우터 추가

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const usersRouter = require('./routes/users.route');
 const postsRouter = require('./routes/posts.route');
+const commentsRouter = require('./routes/comments.route');
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [usersRouter, postsRouter]);
+app.use('/api', [usersRouter, postsRouter, commentsRouter]);
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 실행되었습니다.');
