288d5fa4eacd15b4180eb7571f7415cacaed26e6
wonbin choi <fnffnqls@naver.com>
2023-07-11 12:59:32 +0900

    feat: 게시글 작성, 조회 기능 추가

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -1,5 +1,51 @@
 const express = require('express');
 const { Posts } = require('../models');
+const { Users } = require('../models');
+const authMiddleware = require('../middlewares/auth-middleware');
 const router = express.Router();
+
+// 게시글 작성
+router.post('/posts', authMiddleware, async (req, res) => {
+  const { title, content } = req.body;
+  const { userId } = res.locals.user;
+
+  // 객체의 key를 배열로 반환해서 객체 요소 존재 확인
+  if (Object.keys(req.body).length === 0) {
+    return res.status(412).json({ errorMessage: '데이터 형식이 올바르지 않습니다.' });
+  } else if (title === '') {
+    return res.status(412).json({ errorMessage: '게시글 제목의 형식이 일치하지 않습니다.' });
+  } else if (content === '') {
+    return res.status(412).json({ errorMessage: '게시글 내용의 형식이 일치하지 않습니다.' });
+  }
+
+  try {
+    await Posts.create({ title, content, userId });
+    res.status(201).json({ message: '게시글 작성에 성공하였습니다.' });
+  } catch (error) {
+    res.status(400).json({ errorMessage: '게시글 작성에 실패하였습니다.' });
+    console.log('errorMessage: ' + error.message);
+  }
+});
+
+// 게시글 조회
+router.get('/posts', async (_, res) => {
+  try {
+    const posts = await Posts.findAll({
+      attributes: ['postId', 'userId', 'title', 'createdAt', 'updatedAt'],
+      include: [{ model: Users, as: 'user', attributes: ['nickname'] }],
+      order: [['createdAt', 'desc']],
+    });
+    res.status(200).json({ posts });
+  } catch (error) {
+    res.status(400).json({ errorMessage: '게시물 조회에 실패하였습니다.' });
+    console.log('errorMessage: ' + error.message);
+  }
+});
+
+// 게시글 상세 조회
+
+// 게시글 수정
+
+// 게시글 삭제
 
 module.exports = router;
