7d985298e964e427f6b25db36148d5222731caf1
wonbin choi <fnffnqls@naver.com>
2023-07-11 14:16:58 +0900

    feat: 게시글 상세 조회 기능 추가

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -31,8 +31,11 @@ router.post('/posts', authMiddleware, async (req, res) => {
 router.get('/posts', async (_, res) => {
   try {
     const posts = await Posts.findAll({
+      // 응답할 속성들
       attributes: ['postId', 'userId', 'title', 'createdAt', 'updatedAt'],
+      // join할 테이블과 별명, 응답할 속성
       include: [{ model: Users, as: 'user', attributes: ['nickname'] }],
+      // createdAt 기준으로 나열
       order: [['createdAt', 'desc']],
     });
     res.status(200).json({ posts });
@@ -43,6 +46,21 @@ router.get('/posts', async (_, res) => {
 });
 
 // 게시글 상세 조회
+router.get('/posts/:postId', async (req, res) => {
+  const { postId } = req.params;
+
+  try {
+    const post = await Posts.findOne({
+      attributes: ['postId', 'userId', 'title', 'content', 'createdAt', 'updatedAt'],
+      include: [{ model: Users, as: 'user', attributes: ['nickname'] }],
+      where: { postId },
+    });
+    res.status(200).json({ post });
+  } catch (error) {
+    res.status(400).json({ errorMessage: '게시물 조회에 실패하였습니다.' });
+    console.log('errorMessage: ' + error.message);
+  }
+});
 
 // 게시글 수정
 
