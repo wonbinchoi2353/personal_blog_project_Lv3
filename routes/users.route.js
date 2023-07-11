d121e0901022372282349da61f4f88285bd677cc
wonbin choi <fnffnqls@naver.com>
2023-07-11 21:04:50 +0900

    refactor: 모델 불러오기 수정

--- routes/users.route.js
+++ routes/users.route.js
@@ -84,7 +84,7 @@ router.post('/login', async (req, res) => {
 
   try {
     // email, password 맞을 때 실행, userId를 token에 저장
-    const token = jwt.sign({ userId: user.userId }, env.JWT_KEY_NAME, { expiresIn: '5m' });
+    const token = jwt.sign({ userId: user.userId }, env.JWT_KEY_NAME, { expiresIn: '2h' });
     res.cookie('authorization', `Bearer ${token}`);
     return res.status(200).json({ message: '로그인 성공' });
   } catch (error) {
