47ac2623ac727aa3a4248ff91a89869a9ec2bcdb
wonbin choi <fnffnqls@naver.com>
2023-07-11 11:03:39 +0900

    feat: 콘솔 에러 메세지  추가

--- routes/users.route.js
+++ routes/users.route.js
@@ -53,6 +53,7 @@ router.post('/signup', async (req, res) => {
     return res.status(201).json({ message: '회원 가입에 성공하였습니다.' });
   } catch (error) {
     res.status(400).json({ errorMessage: '요청한 데이터 형식이 올바르지 않습니다.' });
+    console.log(error.errorMessage);
   }
 });
 
@@ -81,6 +82,7 @@ router.post('/login', async (req, res) => {
     return res.status(200).json({ message: '로그인 성공' });
   } catch (error) {
     res.status(400).json({ errorMessage: '로그인에 실패하였습니다.' + error.message });
+    console.log(error.errorMessage);
   }
 });
 
@@ -98,6 +100,7 @@ router.delete('/logout', (req, res) => {
     res.status(200).json({ message: '로그아웃 되었습니다.' });
   } catch (error) {
     res.status(400).json({ errorMessage: error.message });
+    console.log(error.errorMessage);
   }
 });
 
