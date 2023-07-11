c51f1a671328dc7f5359a82cecb63dca0641c50c
wonbin choi <fnffnqls@naver.com>
2023-07-11 12:15:38 +0900

    feat: 쿠키 오류 응답 추가

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -7,6 +7,12 @@ module.exports = async (req, res, next) => {
   try {
     // 객체를 변수로 바꾸기
     const { authorization } = req.cookies;
+
+    // 쿠키가 존재하지 않을 경우
+    if (!authorization) {
+      return res.status(403).json({ errorMessage: '로그인이 필요한 기능입니다.' });
+    }
+
     // 문자열 나눠서 각 변수에 할당
     const [tokenType, token] = authorization.split(' ');
     if (tokenType !== 'Bearer') {
@@ -15,6 +21,7 @@ module.exports = async (req, res, next) => {
 
     // jwt 확인 (token과 비밀 키 사용)
     const decodedToken = jwt.verify(token, env.JWT_KEY_NAME);
+
     // 토큰에 저장했던 userId
     const userId = decodedToken.userId;
     const user = await Users.findOne({ where: { userId } });
@@ -22,12 +29,19 @@ module.exports = async (req, res, next) => {
       res.clearCookie('authorization');
       return res.status(401).json({ message: '토큰 사용자가 존재하지 않습니다.' });
     }
+
     // 미들웨어와 라우터 간에 데이터 전달하기 위한 객체
     res.locals.user = user;
 
     next();
   } catch (error) {
+    // 쿠키가 만료된 경우
+    if (error instanceof jwt.TokenExpiredError) {
+      res.clearCookie('authorization');
+      return res.status(403).json({ errorMessage: '전달된 쿠키에서 오류가 발생하였습니다.' });
+    }
     res.clearCookie('authorization');
-    return res.status(401).json({ message: '비정상적인 요청입니다.' });
+    res.status(401).json({ message: '비정상적인 요청입니다.' });
+    console.log('errorMessage: ' + error.message);
   }
 };
